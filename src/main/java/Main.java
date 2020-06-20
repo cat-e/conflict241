05215c602b192af86c6ca697579d01b3f2b80a25
Ekaterina Hlebalova <khlebalova.e@yandex.ru>
2020-06-21 03:13:17 +0400

    inintial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,77 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        public int yearRevenue(sales) {
+            int sum = 0;
+            for (int monthSale : sales) {
+                sum += monthSale;
+            }
+            return sum;
+        }
+        public int averageMonthlySales(sales){
+            int sum = 0;
+            for (int monthSale : sales) {
+                sum += monthSale;
+            }
+            int averageSale = sum / sales.length;
+            return averageSale;
+        }
+        public int maxMonthSales(sales) {
+            int currentMaxSalesMonth = 1;
+            int currentMax = sales[0];
+            for (int monthSale : sales) {
+                if (currentMax < monthSale) {
+                    currentMax = monthSale;
+                    currentMaxSalesMonth += 1;
+                }
+            }
+            return currentMaxSalesMonth;
+        }
+        public int minMonthSales(sales) {
+            int currentMinSalesMonth = 1;
+            int currentMin = sales[0];
+            for (int monthSale : sales) {
+                if (currentMin > monthSale) {
+                    currentMin = monthSale;
+                    currentMinSalesMonth += 1;
+                }
+            }
+            return currentMinSalesMonth;
+        }
+        public int monthsAmountWithSalesLowerAverage(sales) {
+            int monthsAmount = 1;
+            int sum = 0; // мб не верно
+            for (int monthSale : sales) {
+                sum += monthSale;
+            }
+            int averageSale = sum / sales.length;
+
+
+            for (int monthSale : sales) {
+                if (monthSale < averageSale) {
+                    monthsAmount += 1;
+                }
+            }
+            return monthsAmount;
+        }
+        public int monthsAmountWithSalesHigherAverage(sales) {
+            int monthsAmount = 1;
+            int sum = 0; // мб не верно
+            for (int monthSale : sales) {
+                sum += monthSale;
+            }
+            int averageSale = sum / sales.length;
+
+
+            for (int monthSale : sales) {
+                if (monthSale > averageSale) {
+                    monthsAmount += 1;
+                }
+            }
+            return monthsAmount;
+
+
+        }
+    }
+}
